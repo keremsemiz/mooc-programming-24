c5c72f2169981a27c41e94ba19e9211da0f9ea91
Kerem Semiz <kerox350@gmail.com>
2024-04-17 13:56:21 +0300

    do spruce.py exercise

--- part04-08_spruce/src/spruce.py
+++ part04-08_spruce/src/spruce.py
@@ -1,4 +1,15 @@
 # Write your solution here
+def spruce(size):
+    print("a spruce!")
+    for i in range(1, size + 1):
+        spaces = " " * (size - i)
+        stars = "*" * (2 * i - 1)
+        print(spaces + stars)
+    print(" " * (size - 1) + "*" + " " * (size) )
+
+
+
+
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     spruce(5)
