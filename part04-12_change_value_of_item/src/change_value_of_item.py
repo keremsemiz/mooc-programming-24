2794dced9094edac7788ab1ab54ec6fd9386ebc1
Kerem Semiz <kerox350@gmail.com>
2024-04-17 18:44:30 +0300

    exercise value changer

--- part04-12_change_value_of_item/src/change_value_of_item.py
+++ part04-12_change_value_of_item/src/change_value_of_item.py
@@ -1,6 +1,9 @@
 # Write your solution here
 values = [1, 2, 3, 4, 5]
 while True:
-    ind = int(input("Index:"))
+    ind = int(input("Test"))
+    if ind == -1:
+        break
     val = int(input("New value:"))
-    
+    values[ind] = val
+    print(values)
