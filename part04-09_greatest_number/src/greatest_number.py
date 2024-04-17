81918bd9ecbf1ac9ae74f3e4360a8b1799959022
Kerem Semiz <kerox350@gmail.com>
2024-04-17 18:23:50 +0300

    do exercise greater than

--- part04-09_greatest_number/src/greatest_number.py
+++ part04-09_greatest_number/src/greatest_number.py
@@ -1,5 +1,13 @@
 # Write your solution here
+def greatest_number(a, b, c):
+    numbers = []
+    numbers.append(a)
+    numbers.append(b)
+    numbers.append(c)
+    numbers.sort()
+    return numbers[2]
+    
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
-    greatest = greatest_number(5, 4, 8)
+    greatest = greatest_number(8, 2, 1)
     print(greatest)
