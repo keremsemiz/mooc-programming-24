d7df388e408024253c99b444edc8d665319015cc
Kerem Semiz <kerox350@gmail.com>
2024-04-17 18:27:26 +0300

    do exercise samecharacters

--- part04-10_same_characters/src/same_characters.py
+++ part04-10_same_characters/src/same_characters.py
@@ -1,4 +1,12 @@
 # Write your solution here
+def same_chars(word, in1, in2):
+    if in1 >= len(word) or in2 >= len(word):
+        return False
+    if word[in1] == word[in2]:
+        return True
+    else:
+        return False
+
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
-    print(same_chars("coder", 1, 2))
+    print(same_chars("programmer", 6, 7))
