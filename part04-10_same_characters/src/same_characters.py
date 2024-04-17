a585237db3c8d9dfd18844ad146f6ccb79c65175
Kerem Semiz <kerox350@gmail.com>
2024-04-17 18:27:39 +0300

    fix bug

--- part04-10_same_characters/src/same_characters.py
+++ part04-10_same_characters/src/same_characters.py
@@ -6,7 +6,6 @@ def same_chars(word, in1, in2):
         return True
     else:
         return False
-
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     print(same_chars("programmer", 6, 7))
