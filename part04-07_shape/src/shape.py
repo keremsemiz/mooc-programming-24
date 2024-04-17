fd8fde9651216245546fdcbb3aeb8320b08d4f8b
Kerem Semiz <kerox350@gmail.com>
2024-04-17 13:31:01 +0300

    finish shape exercise

--- part04-07_shape/src/shape.py
+++ part04-07_shape/src/shape.py
@@ -1,4 +1,17 @@
-# Copy here code of line function from previous exercise and use it in your solution
+def line(length, text):
+    if text == "" or text == " ":
+        print("*" * length)
+    else:
+        print(text[0] * length)
+
+def shape(length, letter, height, char):
+    limit = (length - length) + 1
+    for i in range(length):
+        line(limit, letter)
+        limit += 1
+    for i in range(height):
+        line(length, char)
+
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     shape(5, "x", 2, "o")
