a8a7a505cf5f9d3d9f893943f008f47a198e2045
Kerem Semiz <kerox350@gmail.com>
2024-04-17 13:17:37 +0300

    do triangle task

--- part04-06_triangle/src/triangle.py
+++ part04-06_triangle/src/triangle.py
@@ -1,8 +1,14 @@
-# Copy here code of line function from previous exercise
+def line(length, text):
+    if text == "" or text == " ":
+        print("*" * length)
+    else:
+        print(text[0] * length)
 
 def triangle(size):
-    # You should call function line here with proper parameters
-    line(4, "#")
+    limit = (size - size) + 1
+    for i in range(size):
+        line(limit, "#")
+        limit += 1
 
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
