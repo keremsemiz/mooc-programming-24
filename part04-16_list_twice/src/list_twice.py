243563bfee907313703667875cef52d7bf3bb1c6
Kerem Semiz <97160209+keremsemiz@users.noreply.github.com>
2024-04-19 23:29:38 +0200

    TO DO FIX PROBLEM!

--- part04-16_list_twice/src/list_twice.py
+++ part04-16_list_twice/src/list_twice.py
@@ -5,7 +5,6 @@ while True:
     values.append(item)
     print(f"The list now: {values}")
     print(f"The list in order: {values.sort()}")
-
     if item == 0:
         print("Bye!")
         break
