eadffe4ec0981d4aa06ab2e65d4088c506a3dd14
Kerem Semiz <97160209+keremsemiz@users.noreply.github.com>
2024-04-19 23:01:44 +0200

    exercise adding

--- part04-13_add_items_to_list/src/add_items_to_list.py
+++ part04-13_add_items_to_list/src/add_items_to_list.py
@@ -1,1 +1,11 @@
 # Write your solution here
+amount = int(input("How many items:"))
+i = 0
+num = 1
+items = []
+while i < amount:
+    itadd = int(input(f"Item {num}:"))
+    items.append(itadd)
+    num += 1
+    i += 1
+print(items)
