45412ea49e37fbd68a41751d64e9145fa7632d99
Kerem Semiz <97160209+keremsemiz@users.noreply.github.com>
2024-04-19 23:26:26 +0200

    final version

--- part04-16_list_twice/src/list_twice.py
+++ part04-16_list_twice/src/list_twice.py
@@ -2,5 +2,10 @@
 values = []
 while True:
     item = int(input("New item: "))
+    values.append(item)
     print(f"The list now: {values}")
     print(f"The list in order: {values.sort()}")
+
+    if item == 0:
+        print("Bye!")
+        break
