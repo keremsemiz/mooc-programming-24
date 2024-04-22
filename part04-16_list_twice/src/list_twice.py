3969c6abb2deffb26442f1b284450334e2c23b52
Kerem Semiz <97160209+keremsemiz@users.noreply.github.com>
2024-04-22 22:57:42 +0200

    try fix problem

--- part04-16_list_twice/src/list_twice.py
+++ part04-16_list_twice/src/list_twice.py
@@ -1,13 +1,11 @@
 # Write your solution here
 values = []
-sortedlist = None
 while True:
     item = int(input("New item:"))
-    values.append(item)
-    print(f"The list now: {values}")
-    sortedlist = values.sort()
-    print(f"The list in order: {sortedlist}")
-
     if item == 0:
         print("Bye!")
         break
+    values.append(item)
+    print(f"The list now: {values}")
+    sortedvals = sorted(values)
+    print(f"The list in order: {sortedvals}")
