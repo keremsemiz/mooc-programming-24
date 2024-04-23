3f1ba5134ff8cc56e8ecbffd5feac28d155a328a
Kerem Semiz <97160209+keremsemiz@users.noreply.github.com>
2024-04-23 22:52:52 +0200

    finish exercise

--- part04-18_mean/src/mean.py
+++ part04-18_mean/src/mean.py
@@ -2,6 +2,7 @@
 def mean(my_list: list):
     listsum = sum(my_list)
     return listsum / len(my_list)
+
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     my_list = [3, 6, -4]
