feb07770713a8f7402e1c77a7b1f35fdfcae7f78
Kerem Semiz <97160209+keremsemiz@users.noreply.github.com>
2024-04-23 22:55:10 +0200

    fix error

--- part04-19_range_of_list/src/range_of_list.py
+++ part04-19_range_of_list/src/range_of_list.py
@@ -1,6 +1,6 @@
 # Write your solution here
 def range_of_list(my_list: list):
-   retur max(my_list) - min(my_list)
+   return max(my_list) - min(my_list)
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     my_list = [3, 6, -4]
