e53686c57ba1712dbb1ad4b75f0fd2e2e5958a3a
Kerem Semiz <97160209+keremsemiz@users.noreply.github.com>
2024-04-23 22:55:20 +0200

    make code neater

--- part04-19_range_of_list/src/range_of_list.py
+++ part04-19_range_of_list/src/range_of_list.py
@@ -1,6 +1,7 @@
 # Write your solution here
 def range_of_list(my_list: list):
    return max(my_list) - min(my_list)
+
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     my_list = [3, 6, -4]
