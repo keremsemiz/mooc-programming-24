816ad6ba92eedbee169bb1b22b3d8b07eb19db92
Kerem Semiz <97160209+keremsemiz@users.noreply.github.com>
2024-04-23 22:50:28 +0200

    add variable

--- part04-18_mean/src/mean.py
+++ part04-18_mean/src/mean.py
@@ -1,6 +1,6 @@
 # Write your solution here
 def mean(mylist: list):
-    my_list.sum()
+    listsum = my_list.sum()
 # You can test your function by calling it within the following block
 if __name__ == "__main__":
     my_list = [3, 6, -4]
