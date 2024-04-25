f9731c4cb47cd494c8196017fa89ccbd6adb0876
Kerem Semiz <kerox350@gmail.com>
2024-04-25 16:15:56 +0300

    main idea

--- part04-24_palindromes/src/palindromes.py
+++ part04-24_palindromes/src/palindromes.py
@@ -1,4 +1,8 @@
 # Write your solution here
+def palindromes():
+    while True:
+        word = input("Please type in a palindrome:")
+
 # Note, that at this time the main program should not be written inside
 # if __name__ == "__main__":
 # block!
