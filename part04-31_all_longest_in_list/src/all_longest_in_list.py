06540ce9ae5e58bfc580d0cbc4e6c4a731f5c2a1
Kerem Semiz <kerox350@gmail.com>
2024-04-26 13:17:35 +0300

    finish exercise

--- part04-31_all_longest_in_list/src/all_longest_in_list.py
+++ part04-31_all_longest_in_list/src/all_longest_in_list.py
@@ -1,3 +1,19 @@
 # Write your solution here
 def all_the_longest(my_list: list):
-    
+    longest = 0
+    newlist = []
+
+    for i in my_list:
+        if len(i) >= longest:
+            longest = len(i)
+        
+    for i in my_list:
+        if len(i) == longest:
+            newlist.append(i)
+
+    return newlist
+
+if __name__ == "__main__":
+    my_list = ['Alan', 'Steve', 'Seymour']
+    result = all_the_longest(my_list)
+    print(result)
