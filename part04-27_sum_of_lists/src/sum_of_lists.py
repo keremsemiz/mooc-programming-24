4e33eccac1be52be6a760f29f3e782d304f13ea3
Kerem Semiz <kerox350@gmail.com>
2024-04-26 12:53:31 +0300

    finish task

--- part04-27_sum_of_lists/src/sum_of_lists.py
+++ part04-27_sum_of_lists/src/sum_of_lists.py
@@ -1,7 +1,11 @@
 # Write your solution here
 def list_sum(a: list, b: list):
+    newsum = []
     for i in range(len(a)):
-
+        newsum.append(a[i] + b[i])
+    return newsum
 
 if __name__ == "__main__":
-    
+    a = [1, 2, 3]
+    b = [7, 8, 9]
+    print(list_sum(a, b))
