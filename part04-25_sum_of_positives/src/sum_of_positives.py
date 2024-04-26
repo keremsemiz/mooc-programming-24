b6f1787a49306f000af1de9ea6c7396b7ae0e675
Kerem Semiz <kerox350@gmail.com>
2024-04-26 12:42:27 +0300

    fix ideas

--- part04-25_sum_of_positives/src/sum_of_positives.py
+++ part04-25_sum_of_positives/src/sum_of_positives.py
@@ -1,10 +1,12 @@
 # Write your solution here
-def sum_of_positives(mylist: list):
+def sum_of_positives(my_list: list):
     newmylist = []
     for i in my_list:
-        if i < 0:
-            newmylist.append(i)  
+        if i > 0:
+            newmylist.append(i)
+    
+    return sum(newmylist)
 
+    print("The result is", result)
 if __name__ == "__main__":
-    my_list = [1, -2, 3, -4, 5]
-    sum_of_positives(my_list)
+    print(sum_of_positives([1, -1, 2, -2, 3, -3]))
