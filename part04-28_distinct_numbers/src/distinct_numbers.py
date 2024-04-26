b427b961bb879b185f9b7f4db4731fc6b896e33a
Kerem Semiz <kerox350@gmail.com>
2024-04-26 12:57:04 +0300

    do task

--- part04-28_distinct_numbers/src/distinct_numbers.py
+++ part04-28_distinct_numbers/src/distinct_numbers.py
@@ -1,1 +1,11 @@
 # Write your solution here
+def distinct_numbers(my_list: list):
+    for item in my_list:
+        if my_list.count(item) >= 2:
+            my_list.remove(item)   
+            
+    return sorted(my_list)
+
+if __name__ == "__main__":
+    my_list = [3, 2, 2, 1, 3, 3, 1]
+    print(distinct_numbers(my_list))
