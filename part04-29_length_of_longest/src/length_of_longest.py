a21b6031373b24429c278c300e187810b7d27adc
Kerem Semiz <kerox350@gmail.com>
2024-04-26 13:04:57 +0300

    do length_of_longest

--- part04-29_length_of_longest/src/length_of_longest.py
+++ part04-29_length_of_longest/src/length_of_longest.py
@@ -1,1 +1,13 @@
 # Write your solution here
+def length_of_longest(my_list: list):
+    for strings in my_list:
+        length = len(my_list[0])
+        for i in my_list:
+            if len(i) > length:
+                length = len(i)
+    return length
+
+if __name__ == "__main__":
+    my_list = ["first", "second", "fourth", "eleventh"]
+    result = length_of_longest(my_list)
+    print(result)
