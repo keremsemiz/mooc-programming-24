f4eb82693a4af06eaec8392ad1e038bf759b16a7
Kerem Semiz <kerox350@gmail.com>
2024-04-26 12:59:04 +0300

    fix error

--- part04-28_distinct_numbers/src/distinct_numbers.py
+++ part04-28_distinct_numbers/src/distinct_numbers.py
@@ -1,11 +1,12 @@
 # Write your solution here
 def distinct_numbers(my_list: list):
+    results = []
     for item in my_list:
-        if my_list.count(item) >= 2:
-            my_list.remove(item)   
-            
-    return sorted(my_list)
+        if item not in results:
+            results.append(item) 
+
+    return sorted(results)
 
 if __name__ == "__main__":
-    my_list = [3, 2, 2, 1, 3, 3, 1]
+    my_list = [3, 2, 1, 3, 2, 1, 3, 2, 1]
     print(distinct_numbers(my_list))
