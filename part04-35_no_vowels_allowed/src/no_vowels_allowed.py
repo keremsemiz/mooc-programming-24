22293f8e7db1f317aac2335b332f5febea6a5494
Kerem Semiz <kerox350@gmail.com>
2024-04-26 14:17:18 +0300

    do no vowels ex

--- part04-35_no_vowels_allowed/src/no_vowels_allowed.py
+++ part04-35_no_vowels_allowed/src/no_vowels_allowed.py
@@ -1,1 +1,14 @@
 # Write your solution here
+def no_vowels(string):
+    vowels = ["a", "e", "i", "o", "u"]
+    word = ""
+    for char in string:
+        if char in vowels:
+            char = ""
+        word += char
+    
+    return word
+
+if __name__ == "__main__":
+    my_string = "this is an example"
+    print(no_vowels(my_string))
