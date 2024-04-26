e7f5bb73f86e21fb11ac53aaed08d358fcf000cb
Kerem Semiz <kerox350@gmail.com>
2024-04-26 13:31:38 +0300

    do exercise integers to strings

--- part04-32_integers_to_strings/src/integers_to_strings.py
+++ part04-32_integers_to_strings/src/integers_to_strings.py
@@ -1,1 +1,12 @@
 # Write your solution here
+def formatted(my_list: list):
+    newlist = []
+    for i in my_list:
+        num = "{:.2f}".format(i)
+        newlist.append(num)
+    return newlist
+
+if __name__ == "__main__":
+    my_list = [1.234, 0.3333, 0.11111, 3.446]
+    new_list = formatted(my_list)
+    print(new_list)
