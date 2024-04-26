f2fe297f833bdf71d140837ce8b2ced2ebeec276
Kerem Semiz <kerox350@gmail.com>
2024-04-26 14:12:34 +0300

    do most common character exercise

--- part04-34_most_common_character/src/most_common_character.py
+++ part04-34_most_common_character/src/most_common_character.py
@@ -1,1 +1,16 @@
 # Write your solution here
+def most_common_character(string):
+    mcc = 0
+    mccl = 0
+    for char in string:
+        if string.count(char) > mccl:
+            mccl = string.count(char)
+            mcc = char
+    return mcc
+
+if __name__ == "__main__":
+    first_string = "abcdbde"
+    print(most_common_character(first_string))
+
+    second_string = "exemplaryelementary"
+    print(most_common_character(second_string))
