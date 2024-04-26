767dd96db719da325eb13dabbb85fe8ff30b3f61
Kerem Semiz <kerox350@gmail.com>
2024-04-26 12:39:12 +0300

    add basic outline

--- part04-25_sum_of_positives/src/sum_of_positives.py
+++ part04-25_sum_of_positives/src/sum_of_positives.py
@@ -1,1 +1,10 @@
 # Write your solution here
+def sum_of_positives(mylist: list):
+    newmylist = []
+    for i in my_list:
+        if i < 0:
+            newmylist.append(i)  
+
+if __name__ == "__main__":
+    my_list = [1, -2, 3, -4, 5]
+    sum_of_positives(my_list)
