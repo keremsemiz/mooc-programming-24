5bd7aefcd8dc70b07f608571ba2e396e40dc348b
Kerem Semiz <kerox350@gmail.com>
2024-04-26 14:06:59 +0300

    do everything reversed exericse

--- part04-33_everything_reversed/src/everything_reversed.py
+++ part04-33_everything_reversed/src/everything_reversed.py
@@ -1,1 +1,11 @@
 # Write your solution here
+def everything_reversed(my_list: list):
+    revlist = []
+    for item in my_list:
+        revlist.insert(0, item[::-1])
+    return revlist
+
+if __name__ == "__main__":
+    my_list = ["Hi", "there", "example", "one more"]
+    new_list = everything_reversed(my_list)
+    print(new_list)
