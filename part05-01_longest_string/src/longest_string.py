a8b3a1b9255e2d55d942b82538cadaa7c9e747d8
Kerem Semiz <kerox350@gmail.com>
2024-04-28 19:43:59 +0300

    do main outline

--- part05-01_longest_string/src/longest_string.py
+++ part05-01_longest_string/src/longest_string.py
@@ -1,1 +1,11 @@
 # Write your solution here
+def longest(strings: list):
+    longest = strings[0]
+    for word in strings:
+        if len(word) > longest:
+            longest = word
+    return longest
+
+if __name__ == "__main__":
+    strings = ["hi", "hiya", "hello", "howdydoody", "hi there"]
+    print(longest(strings))
