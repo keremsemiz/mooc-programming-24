52265fd4291053e6720f11124a7b2b24c2b395ad
Kerem Semiz <kerox350@gmail.com>
2024-05-07 18:54:31 +0300

    add mainn outline

--- part05-02_number_of_elements/src/number_of_elements.py
+++ part05-02_number_of_elements/src/number_of_elements.py
@@ -1,1 +1,11 @@
 # Write your solution here
+def count_matching_elements(my_matrix: list, element: int):
+    for element in my_matrix:
+        
+
+
+
+
+if __name__ == "__main__":
+    m = [[1, 2, 1], [0, 3, 4], [1, 0, 0]]
+    print(count_matching_elements(m, 1))
