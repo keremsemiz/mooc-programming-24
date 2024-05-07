77be8979d8a70a5986f0b72aa7f998d09c806c61
Kerem Semiz <kerox350@gmail.com>
2024-05-07 18:57:07 +0300

    finish exercise

--- part05-02_number_of_elements/src/number_of_elements.py
+++ part05-02_number_of_elements/src/number_of_elements.py
@@ -1,11 +1,12 @@
 # Write your solution here
 def count_matching_elements(my_matrix: list, element: int):
-    for element in my_matrix:
-        
+    amt = []
+    for part in my_matrix:
+        add = part.count(element)
+        amt.append(add)
+    sm = sum(amt)
 
-
-
-
+    return sm    
 if __name__ == "__main__":
     m = [[1, 2, 1], [0, 3, 4], [1, 0, 0]]
     print(count_matching_elements(m, 1))
