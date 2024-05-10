9d1eefec18f0a66a3d1c91d2a426dd0aa798a8c8
Kerem Semiz <kerox350@gmail.com>
2024-05-10 14:37:17 +0300

    finish phonebook part 1

--- part05-17_phone_book_v1/src/phone_book_v1.py
+++ part05-17_phone_book_v1/src/phone_book_v1.py
@@ -1,1 +1,21 @@
 # Write your solution here
+phonebook = {}
+
+while True:
+    cmd = int(input("command (1 search, 2 add, 3 quit):"))
+    if cmd == 3:
+        print("quitting...")
+        break
+    name = str(input("name:"))
+    if cmd == 2:
+        number = input("number:")
+        print("ok!")
+
+
+    if cmd == 2:
+        phonebook[name] = number
+    elif cmd == 1:
+        if name not in phonebook:
+            print("no number")
+        else:
+            print(phonebook[name])
