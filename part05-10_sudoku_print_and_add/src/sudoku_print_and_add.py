5b8d16444bc7f49d126d088c86a0f8d706f14768
Kerem Semiz <kerox350@gmail.com>
2024-05-10 13:27:03 +0300

    forgot to add newline

--- part05-10_sudoku_print_and_add/src/sudoku_print_and_add.py
+++ part05-10_sudoku_print_and_add/src/sudoku_print_and_add.py
@@ -1,1 +1,45 @@
 # Write your solution here
+def print_sudoku(sudoku: list):
+    for i in range(9):
+        for j in range(9):
+            if sudoku[i][j] == 0:
+                sudoku[i][j] = "_"
+
+    newsdku = sudoku[:]
+
+    for newRow in range(9):
+        if newRow > 0 and newRow % 3 == 0:
+            print()
+        
+        for newCol in range(9):
+            print(newsdku[newRow][newCol], end=" ")
+            if (newCol + 1) % 3 == 0:
+                print(end=" ")
+        print()
+
+
+def add_number(sudoku: list, row_no: int, column_no: int, number:int):
+    sudoku[row_no][column_no] = number
+
+
+if __name__ == "__main__":
+    sudoku  = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+
+    print_sudoku(sudoku)
+    add_number(sudoku, 0, 0, 2)
+    add_number(sudoku, 1, 2, 7)
+    add_number(sudoku, 5, 7, 3)
+    print()
+    print("Three numbers added:")
+    print()
+    print_sudoku(sudoku)
