b3dd2da96ac12a6888ec7becf51ffdc2fd6130fa
Kerem Semiz <kerox350@gmail.com>
2024-05-10 14:16:10 +0300

    do factorials exercise

--- part05-15_factorials/src/factorials.py
+++ part05-15_factorials/src/factorials.py
@@ -1,1 +1,18 @@
 # Write your solution here
+def factorise(num: int):
+    for i in range(1, num):
+        num *= i
+    return num
+
+def factorials(n: int):
+    newdict = {}
+    for i in range(1, n + 1):
+        newdict[i] = factorise(i)
+
+    return newdict
+
+if __name__ == "__main__":
+    k = factorials(5)
+    print(k[1])
+    print(k[3])
+    print(k[5])
