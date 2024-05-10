921c661e577e2063be6cd25c2c7a864ea856963e
Kerem Semiz <kerox350@gmail.com>
2024-05-10 13:59:47 +0300

    do first dictionary exercise

--- part05-14_times_ten/src/times_ten.py
+++ part05-14_times_ten/src/times_ten.py
@@ -1,1 +1,11 @@
 # Write your solution here
+def times_ten(start_index: int, end_index: int):
+    newdict = {}
+    for i in range(start_index, end_index + 1):
+        newdict[i] = i * 10
+    
+    return newdict
+
+if __name__ == "__main__":
+    d = times_ten(3, 6)
+    print(d)
