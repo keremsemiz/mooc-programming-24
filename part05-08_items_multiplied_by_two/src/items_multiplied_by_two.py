2578db8e2bcc419232dadb4065af5f82cf62eb1c
Kerem Semiz <kerox350@gmail.com>
2024-05-10 13:16:12 +0300

    do simple doubling exercise

--- part05-08_items_multiplied_by_two/src/items_multiplied_by_two.py
+++ part05-08_items_multiplied_by_two/src/items_multiplied_by_two.py
@@ -1,1 +1,15 @@
 # Write your solution here
+def double_items(numbers: list):
+    numbers_doubled = []
+
+    for number in numbers:
+        numbers_doubled.append(number * 2)
+    
+    return numbers_doubled
+
+
+if __name__ == "__main__":
+    numbers = [2, 4, 5, 3, 11, -4]
+    numbers_doubled = double_items(numbers)
+    print("original:", numbers)
+    print("doubled:", numbers_doubled)
