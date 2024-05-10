c7b1fe225941b007e3a6f04ff7d28707ce3c3e02
Kerem Semiz <kerox350@gmail.com>
2024-05-10 14:49:10 +0300

    do phonebook version 2

--- part05-18_phone_book_v2/src/phone_book_v2.py
+++ part05-18_phone_book_v2/src/phone_book_v2.py
@@ -1,1 +1,33 @@
 # Write your solution here
+def search(phonebook):
+    name = input("name:")
+    if name not in phonebook:
+        print("no number")
+    else:
+        for num in phonebook[name]:
+            print(num)
+
+def add(phonebook):
+    name = input("name: ")
+    number = input("number: ")
+
+    if name not in phonebook:
+        phonebook[name] = []
+    
+    phonebook[name].append(number)
+    print("ok!")
+
+def main():
+    phonebook = {}  
+    while True: 
+        cmd = int(input("command (1 search, 2 add, 3 quit):"))
+        if cmd == 1:
+            search(phonebook)
+        if cmd == 2:
+            add(phonebook)
+        if cmd == 3:
+            break
+    print("quitting...")
+
+main()
+
