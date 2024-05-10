220ca3100f4ea5f21f21e710b2364ad03f0b399d
Kerem Semiz <kerox350@gmail.com>
2024-05-10 13:18:17 +0300

    do easy exercise

--- part05-09_remove_smallest/src/remove_smallest.py
+++ part05-09_remove_smallest/src/remove_smallest.py
@@ -1,1 +1,9 @@
 # Write your solution here
+def remove_smallest(numbers: list):
+    m = min(numbers)
+    numbers.remove(m)
+
+if __name__ == "__main__":
+    numbers = [2, 4, 6, 1, 3, 5]
+    remove_smallest(numbers)
+    print(numbers)
