3fcc13859f7bd315ea45c1bbe873d3130d69e99c
Kerem Semiz <kerox350@gmail.com>
2024-05-10 14:29:49 +0300

    do histogram exercise

--- part05-16_histogram/src/histogram.py
+++ part05-16_histogram/src/histogram.py
@@ -1,1 +1,15 @@
 # Write your solution here
+def histogram(string: str):
+    amount = {}
+    for i in string:
+        if i not in amount:
+            amount[i] = 0
+        amount[i] += 1
+
+    for key, value in amount.items():
+        print(f"{key} {value* "*"}")
+
+
+
+if __name__ == "__main__":
+    histogram("statistically")
