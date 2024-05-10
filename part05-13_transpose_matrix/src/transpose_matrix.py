65682e2b7dacb4a9b759da1259123b1e5cb6eece
Kerem Semiz <kerox350@gmail.com>
2024-05-10 13:54:44 +0300

    do matrix transpose exercise

--- part05-13_transpose_matrix/src/transpose_matrix.py
+++ part05-13_transpose_matrix/src/transpose_matrix.py
@@ -1,1 +1,13 @@
 # Write your solution here
+def transpose(matrix: list):
+    for row in range(len(matrix)):
+        for column in range(row, len(matrix)):
+            num = matrix[row][column]
+            matrix[row][column] = matrix[column][row]
+            matrix[column][row] = num
+
+
+if __name__ == "__main__":
+    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    print(matrix)
+    transpose(matrix)
