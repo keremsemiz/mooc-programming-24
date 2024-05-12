e6d850602c586e5c3df4b3ed8c18c3cbce0b2090
Kerem Semiz <kerox350@gmail.com>
2024-05-12 13:11:32 +0300

    do create tuple task

--- part05-23_create_tuple/src/create_tuple.py
+++ part05-23_create_tuple/src/create_tuple.py
@@ -1,1 +1,18 @@
 # Write your solution here
+def create_tuple(x: int, y: int, z: int):
+    lst = []
+    lst.append(x)
+    lst.append(y)
+    lst.append(z)
+    lst.sort()
+    min = lst[0]
+    max = lst[2]
+
+    sm = x + y + z
+
+    tpl = (min, max, sm)
+
+    return tpl
+
+if __name__ == "__main__":
+    print(create_tuple(5, 3, -1))
