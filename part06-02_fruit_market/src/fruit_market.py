74534e69573e15e41f718605be76f4fc206bc0e1
Kerem Semiz <kerox350@gmail.com>
2024-05-18 12:59:00 +0300

    do csv reading task

--- part06-02_fruit_market/src/fruit_market.py
+++ part06-02_fruit_market/src/fruit_market.py
@@ -1,1 +1,13 @@
 # write your solution here
+def read_fruits():
+    frtdict = {}
+    with open("fruits.csv") as csvfile:
+        for line in csvfile:
+            line = line.replace("\n", "")
+            parts = line.split(";")
+            frtdict[parts[0]] = float(parts[1]) 
+    
+    return frtdict
+
+if __name__ == "__main__":
+    print(read_fruits())
