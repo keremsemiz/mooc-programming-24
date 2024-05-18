c2be3f7d51bbe1ade8db372caadc52426918158e
Kerem Semiz <kerox350@gmail.com>
2024-05-18 11:42:27 +0300

    do largest number reading task

--- part06-01_largest_number/src/largest_number.py
+++ part06-01_largest_number/src/largest_number.py
@@ -1,2 +1,13 @@
 # write your solution here
+def largest():
+    with open("numbers.txt") as file:
+        numbers = []
+        for num in file:
+            numbers.append(int(num))
 
+    if len(numbers) > 0:
+        numbers[0] = max(numbers)
+    print(numbers[0])
+
+if __name__ == "__main__":
+    largest()
