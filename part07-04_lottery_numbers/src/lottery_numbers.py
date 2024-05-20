654a5aaa5819f0b2e21c497f5f52086f770bbe4c
Kerem Semiz <kerox350@gmail.com>
2024-05-20 12:41:50 +0300

    do lottery number picking task with random module

--- part07-04_lottery_numbers/src/lottery_numbers.py
+++ part07-04_lottery_numbers/src/lottery_numbers.py
@@ -1,1 +1,18 @@
 # Write your solution here
+from random import randint
+
+def lottery_numbers(amount: int, lower: int, upper: int):
+    nums = []
+    while len(nums) < amount:
+        pick = randint(lower, upper)
+        if pick not in nums:
+            nums.append(pick)
+        else:
+            continue
+
+    return sorted(nums)
+
+
+if __name__ == "__main__":
+    for number in lottery_numbers(7, 1, 40):
+        print(number)
