9023dda716d85e4b23fab78b2e8355f93db1c1d8
Kerem Semiz <kerox350@gmail.com>
2024-05-20 10:42:40 +0300

    complete fraction exercise

--- part07-03_fractions/src/calculation_with_fractions.py
+++ part07-03_fractions/src/calculation_with_fractions.py
@@ -1,1 +1,17 @@
 # Write your solution here
+from fractions import Fraction
+
+def fractionate(amount: int):
+    frc = []
+    for i in range(amount):
+        frc.append(Fraction(1, amount))
+
+    return frc
+
+if __name__ == "__main__":
+    for p in fractionate(3):
+        print(p)
+
+    print()
+
+    print(fractionate(5))
