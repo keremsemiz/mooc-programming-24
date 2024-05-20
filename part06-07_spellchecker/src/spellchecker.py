863f7d9214247bc8f49532534d32a26597758639
Kerem Semiz <kerox350@gmail.com>
2024-05-20 10:15:13 +0300

    do stuff

--- part06-07_spellchecker/src/spellchecker.py
+++ part06-07_spellchecker/src/spellchecker.py
@@ -1,1 +1,21 @@
 # write your solution here
+with open("wordlist.txt") as wordlist:
+    wordlistASLIST = []
+    for line in wordlist:
+        line = line.replace("\n", "")
+        wordlistASLIST.append(line)
+        
+    text = input("Write text: ")
+    textnospace = text.split(' ')
+    final = ''
+
+    for index, word in enumerate(wordlist):
+        wordlist[index] = word.lower()
+
+    for text in textnospace:
+        if text.lower() in wordlist:
+            final += text
+        else:
+            final += "*" + text + "*"
+
+print(final)
