6f56c10239c81ef8c78230c13484e127b1c710ed
Kerem Semiz <kerox350@gmail.com>
2024-05-20 10:22:30 +0300

    do special characters exercise

--- part07-02_special_characters/src/special_characters.py
+++ part07-02_special_characters/src/special_characters.py
@@ -1,1 +1,23 @@
 # Write your solution here
+from string import ascii_letters, punctuation
+
+def separate_characters(my_string: str):
+    first = ''
+    second = ''
+    third = ''
+
+    for char in my_string:
+        if char in ascii_letters:
+            first += char
+        elif char in punctuation:
+            second += char
+        else:
+            third += char
+
+    return(first, second, third)
+
+if __name__ == "__main__":
+    parts = separate_characters("Olé!!! Hey, are ümläüts wörking?")
+    print(parts[0])
+    print(parts[1])
+    print(parts[2])
