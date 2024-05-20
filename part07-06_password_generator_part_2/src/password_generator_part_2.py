bfbcf974f85ec342ad7b962f5ca745a70edab2d8
Kerem Semiz <kerox350@gmail.com>
2024-05-20 13:05:04 +0300

    do password generator last part

--- part07-06_password_generator_part_2/src/password_generator_part_2.py
+++ part07-06_password_generator_part_2/src/password_generator_part_2.py
@@ -1,1 +1,19 @@
 # Write your solution here
+from random import randint, choice
+from string import ascii_letters
+
+def generate_strong_password(length: int, secondArgument: bool, thirdArgument: bool):
+    specialChars = "!?=+-()#"
+    output = ''
+    while len(output) < length:
+        if len(output) < length:
+            output += choice(ascii_letters.lower())
+        if secondArgument and len(output) < length:
+            output += str(randint(0, 9))
+        if thirdArgument and len(output) < length:
+            output += choice(specialChars)
+    
+    return output
+
+if __name__ == "__main__":
+    print(generate_strong_password(8, True, True))
