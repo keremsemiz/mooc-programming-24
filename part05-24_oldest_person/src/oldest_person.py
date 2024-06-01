fae4aa691f749717427c076c4ad6804045de99fa
Kerem Semiz <kerox350@gmail.com>
2024-06-01 14:03:05 +0300

    do oldest person exercise

--- part05-24_oldest_person/src/oldest_person.py
+++ part05-24_oldest_person/src/oldest_person.py
@@ -1,16 +1,20 @@
-# Write your solution here
-def oldest_person(people: list):
-    for i in people:
-        oldest = max(people[i][1])
-
-    return oldest[0]
+def oldest_person(people: list) -> str:
+    oldest_name = people[0][0]
+    oldest_year = people[0][1]
+    
+    for person in people[1:]:
+        name, birth_year = person
+        if birth_year < oldest_year:
+            oldest_name = name
+            oldest_year = birth_year
+    
+    return oldest_name
 
 if __name__ == "__main__":
     p1 = ("Adam", 1977)
     p2 = ("Ellen", 1985)
     p3 = ("Mary", 1953)
     p4 = ("Ernest", 1997)
-    p5 = ('Emily', 2014)
     people = [p1, p2, p3, p4]
 
     print(oldest_person(people))
