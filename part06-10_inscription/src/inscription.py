3d33d120f82f18eb327e307958660f6214279db7
Kerem Semiz <kerox350@gmail.com>
2024-06-01 19:30:54 +0300

    do file writing exercise

--- part06-10_inscription/src/inscription.py
+++ part06-10_inscription/src/inscription.py
@@ -1,1 +1,6 @@
 # Write your solution here
+name = input("Whom should I sign this to: ")
+file = input("Where shall I save it: ")
+
+with open(file, "w") as inscribedfile:
+    inscribedfile.write(f"Hi {name}, we hope you enjoy learning Python with us! Best, Mooc.fi Team")
