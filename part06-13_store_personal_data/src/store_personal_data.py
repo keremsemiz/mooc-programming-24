076f6dc55ed25e6f8d4d0f42db16bc33d29518b4
Kerem Semiz <kerox350@gmail.com>
2024-06-02 14:28:42 +0300

    do exercise

--- part06-13_store_personal_data/src/store_personal_data.py
+++ part06-13_store_personal_data/src/store_personal_data.py
@@ -1,1 +1,9 @@
 # Write your solution here
+def store_personal_data(person: tuple):
+    with open("people.csv", 'a') as newfile:
+        newfile.write(f"{person[0]};{person[1]};{person[2]}")
+        newfile.close()
+
+
+if __name__ == "__main__":
+    store_personal_data("Paul Paulson", 37, 175.5)
